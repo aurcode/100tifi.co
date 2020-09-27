29e940ada6330a4f06370f6346c2041a542f79bb
aurcode <aurelio3927@gmail.com>
2020-09-27 19:59:04 -0400

    Get characters with the API call function

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,19 @@
-const Home = () => {
+import getData from '../utils/getData';
+
+const Home = async () => {
+  const characters = await getData();
   const view = `
-  <div class='Characters'>
-    <article class='Characters-item'>
-      <a href='#/1/'>
-        <img src='image' alt='name'/>
-        <h2>Name</h2>
-      </a>
-    </article>
-  </div>`;
+    <div class='Characters'>
+      ${characters.results.map(character => `
+          <article class='Characters-item'>
+            <a href='#/${characters.id}/'>
+              <img src='${character.image}' alt='Foto de ${character.name}'/>
+              <h2>${character.name}</h2>
+            </a>
+          </article>
+        `).join('')}
+    </div>
+  `;
   return view;
 }
 
